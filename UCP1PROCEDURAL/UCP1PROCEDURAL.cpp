e47690202a0b98e4012f203fdd22597814c2044c
firyalshafa <shafafiryal782@gmail.com>
2024-05-05 12:23:39 +0700

    ucp 1

--- UCP1PROCEDURAL/UCP1PROCEDURAL.cpp
+++ UCP1PROCEDURAL/UCP1PROCEDURAL.cpp
@@ -2,19 +2,97 @@
 //
 
 #include <iostream>
+using namespace std;
+
+
+struct node {
+    string nama;
+    string nilaibahasainggris;
+    string nilaimatematika;
+    string status;
+};
+
+
+
+void addnode() {
+    string nama;
+    string nilaibahasainggris;
+    string nilaimatematika;
+    string status;
+    cout << "masukan nama:";
+    cin >> nama;
+    cout << "masukan nilai bahasa inggris:";
+    cin >> nilaibahasainggris;
+    cout << "masukan nilai matematika:";
+    cin >> nilaimatematika;
+
+    if (nilaimatematika <= 80)
+    {
+        cout << "status lulus" << endl;
+        cin >> status;
+    }
+
+    if (nilaibahasainggris & nilaimatematika > 70)
+    {
+        cout << "status lulus" << endl;
+        cin >> status;
+    }
+
+    if (nilaimatematika & nilaibahasainggris < 70)
+    {
+        cout << "status tidak lulus" << endl;
+        cin >> status;
+    }
+
+}
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "masukan nama " << endl;
+    cout << "masukan nilai matematika " << endl;
+    cout << " masukan nilai bahasa inggris " << endl;
+    cout << "tampilkan status" << endl;
+
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+// jawaban ucp no 1-5
+//   1.implementasi variabel 
+// int ctr;
+// char ch;
+// int item;
+//   2. implementasi conditional stetement
+//   if (nilaimatematika & nilaibahasainggris < 70)
+// { cout << " masukan nilai matematika "
+// cin }
+//   3. implementasi srtuct 
+// 
+//   4. implementasi fungsi 
+//      switch(kondisi)
+//       {
+//        case konstanta-1=-----
+//         break;
+//         case konstanta-2=-----
+//            break;
+//         default = 
+//           break;
+//           }
+// implementasi prosedur 
+//         void display()
+// {
+// cout << "array elements are:";
+// for (i=0;<n; i++)
+// cout << arr[i]<<""'
+// cout << endl;
+//   5.implementasi looping
+// do....)
+// {
+// 
+// }while 
+
+
